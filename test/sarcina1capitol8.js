0046cbe8f5de732c9f009acec00dd0879bef0873
savu filip <savufilip@gmail.com>
2025-03-11 17:05:13 +0200

    adaugare

--- test/sarcina1capitol8.js
+++ test/sarcina1capitol8.js
@@ -1,14 +1,14 @@
 // Selectăm toate elementele li cu clasa "item" din ul#categories
 const categories = document.querySelectorAll('#categories .item');
 
-// Afișăm numărul de categorii
+// Afisam nr de categorii
 console.log(`Number of categories: ${categories.length}`);
 
-// Pentru fiecare categorie, obținem titlul și numărul de elemente din lista internă
+// Pentru fiecare categorie, obtinem titlul și numarul de elemente
 categories.forEach(category => {
-  // Extragem textul din tag-ul <h2>
+  // Extragem textul din tag-ul h2
   const title = category.querySelector('h2').textContent;
-  // Numărăm elementele din lista internă (li din ul)
+  // Numaram elem din lista interna (li din ul)
   const elementsCount = category.querySelector('ul').children.length;
   
   console.log(`Category: ${title}`);
